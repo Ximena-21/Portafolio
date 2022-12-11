37246880036cffce081bb4f8bdcb02b287455f87
Ximena-21 <ximenanzola.21@gmail.com>
2022-12-11 18:09:36 -0500

    read md projects

--- content/projects/trivia-name.md
+++ content/projects/trivia-name.md
@@ -5,7 +5,7 @@ image: /uploads/download.jpeg
 github: https://github.com/Ximena-21
 external: algo
 mockup: otro
-date: 2022-12-11T21:26:41.543Z
+# date: 2022-12-11T21:26:41.543Z
 tecnologies:
   - JavaScript
 ---
--- pages/testpage.tsx
+++ pages/testpage.tsx
@@ -1,7 +1,33 @@
-export default function TestPage () {
+import {readFileSync} from "fs"
+import matter from 'gray-matter'
+
+export default function TestPage (props: any) {
+
+    console.log(props)
+
+    // getStaticProps()
     return (
         <div>
             <h1>hola!!</h1>
+            <p>{props.parseData?.title}</p>
+            <p>{props.parseData?.description}</p>
         </div>
     )
 }
+
+export async function getStaticProps() {
+    const fileContent = readFileSync("content/projects/trivia-name.md", {encoding: "utf-8"})
+
+    const parseData = matter(fileContent).data
+
+    // console.log(fileContent);
+
+    return {
+        props: {
+            fileContent,
+            parseData
+        }
+    }
+
+}
+
